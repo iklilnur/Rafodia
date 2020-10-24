39265b418fa1a608159e1c0ae55e1021fc4688d3
iklilnur <iklilnur@gmail.com>
2020-10-24 11:15:00 +0700

    Initial commit

---
+++ app/src/main/java/com/example/rutepantry/FilledGroceries.java
@@ -0,0 +1,14 @@
+package com.example.rutepantry;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FilledGroceries extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filled_groceries);
+    }
+}
---
+++ app/src/main/java/com/example/rutepantry/InputGroceries.java
@@ -0,0 +1,14 @@
+package com.example.rutepantry;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InputGroceries extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input_groceries);
+    }
+}
