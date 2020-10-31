2d5ded1433b553c041e09cda97121801334a2eb5
syafiranm <syafiranurilhaq@gmail.com>
2020-10-31 22:46:16 +0700

    nambah activity foodpedia

---
+++ app/src/main/java/com/example/rutepantry/Foodpedia_category.java
@@ -0,0 +1,14 @@
+package com.example.rutepantry;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Foodpedia_category extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_foodpedia_category);
+    }
+}
