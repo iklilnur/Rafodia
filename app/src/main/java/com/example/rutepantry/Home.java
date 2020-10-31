1e79731141f2df4cce35573de6382a0916cbb26d
iklilnur <iklilnur@gmail.com>
2020-10-31 22:42:54 +0700

    cowmeat ganti beef

--- app/src/main/java/com/example/rutepantry/Home.java
+++ app/src/main/java/com/example/rutepantry/Home.java
@@ -2,13 +2,45 @@ package com.example.rutepantry;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Home extends AppCompatActivity {
+
+    Button food_pedia;
+    Button pantry_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        food_pedia = (Button) findViewById(R.id.foodpediaButton);
+        pantry_button = (Button) findViewById(R.id.pantryButton);
+
+        pantry_button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                pantry();
+            }
+        });
+
+        food_pedia.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                foodPedia();
+            }
+        });
+    }
+
+    public void foodPedia(){
+
+    }
+
+    public void pantry(){
+        Intent intent = new Intent(this, Pantry.class);
+        startActivity(intent);
     }
 }
